9ea0825e1e5eba77377ef80630f52e380118b050
caiomagri <kaioeduardofm@gmail.com>
2020-02-10 21:13:44 -0300

    [IMP] Adicionando styled components para search de repositorios

---
+++ src/pages/Main/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
+
+import { Container, Form, SubmitButton } from './styles';
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newRepo: '',
+      repositories: [],
+      loading: false,
+    };
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const { newRepo, repositories } = this.state;
+
+    this.setState({ loading: true });
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    this.setState({ loading: false });
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({ repositories: [...repositories, data], newRepo: '' });
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositorios
+        </h1>
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar Repositorio"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
+}
